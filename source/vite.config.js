e5949eeb91360220a4185fa4386a08e6b23704d3
T1GER <info@t1ger.net>
2026-09-21 10:07:37 +0200

    refactor(ui): build outDir

--- source/vite.config.js
+++ source/vite.config.js
@@ -1,7 +1,13 @@
-import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+
+  base: './',
+
+  build: {
+    outDir: '../web',
+    emptyOutDir: true,
+  },
 })
