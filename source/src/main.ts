c8fe469dfe55c4e9d092c33667fd8ea9bbfd0ae2
T1GER <info@t1ger.net>
2026-09-21 10:03:56 +0200

    clean init

--- source/src/main.ts
+++
@@ -1,7 +0,0 @@
-import '@fontsource-variable/inter'
-import 'leaflet/dist/leaflet.css'
-import '@/assets/main.css'
-import { createApp } from 'vue'
-import App from '@/App.vue'
-
-createApp(App).mount('#app')
